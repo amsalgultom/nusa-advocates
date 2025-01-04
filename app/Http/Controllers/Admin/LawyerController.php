6d881c48f8b92d553130f10567d238c98e561f71
Amsal Gultom <gultomamsal@gmail.com>
2025-01-04 14:55:55 +0700

    add logger

--- app/Http/Controllers/Admin/LawyerController.php
+++ app/Http/Controllers/Admin/LawyerController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Services\LoggerService;
 use App\Models\Lawyer;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -11,6 +12,12 @@ use Illuminate\Support\Facades\Storage;
 
 class LawyerController extends Controller
 {
+    protected $logger;
+
+    public function __construct(LoggerService $logger)
+    {
+        $this->logger = $logger;
+    }
     /**
      * Display a listing of the resource.
      */
@@ -71,6 +78,10 @@ class LawyerController extends Controller
              Lawyer::create($request->except('image') + ['image' => $imageName ?? null]);
              return redirect()->route('lawyer.index')->with('success', 'Created New Franchise Successfully.');
          } catch (\Exception $e) {
+             //send to log provider
+             $message = $e->getMessage();
+             $this->logger->logMessage($message);
+ 
              return redirect()->back()->with('error', 'An error occurred while create the lawyer.');
          }
      }
@@ -113,6 +124,10 @@ class LawyerController extends Controller
  
              return redirect()->route('lawyer.index')->with('success', 'Updated Franchise Successfully.');
          } catch (\Exception $e) {
+             //send to log provider
+             $message = $e->getMessage();
+             $this->logger->logMessage($message);
+ 
              return redirect()->back()->with('error', 'An error occurred while update the Franchise.');
          }
      }
