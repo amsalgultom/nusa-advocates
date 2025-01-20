39dd55a9ecd65c3a3d82bbab426b10bc70fde571
Amsal Gultom <gultomamsal@gmail.com>
2025-01-20 22:14:59 +0700

    update content views news

--- resources/views/pages/news.blade.php
+++ resources/views/pages/news.blade.php
@@ -2,57 +2,22 @@
 @section('title', 'Nusa Advocates | '. __('messages.news'))
 
 @section('content')
-<!-- ======= Hero Section ======= -->
-<section id="hero">
-  <div id="heroCarousel" data-bs-interval="5000" class="carousel slide carousel-fade" data-bs-ride="carousel">
-    <div id="hero-carousel-indicators"></div>
-    <div class="carousel-inner" role="listbox">
-      <div class="carousel-item active" style="background-image: url({{ asset('img/banner/nusa_hero_2.jpg') }})">
-        <div class="carousel-container">
-          <div class="container">
-            <h2>@lang('messages.news')</h2>
-            <!-- <p>
-            @lang('messages.short_news')
-            </p> -->
-          </div>
+<main id="main">  <!-- ======= Nusa Advocates Section ======= -->
+  <section id="Nusa Advocates" class="Nusa Advocates mt-5">
+  @foreach($news as $new)
+    <div class="container mt-4">
+      <div class="row">
+        <div class="col-lg-12">
+          <h4 class="title-newsdetail">{{ $new->title }}</h4>
+          <p>{{ date('d F Y', strtotime($new->date )) }}</p>
+          <span class="desc-newsdetail">
+            {!! $new->desc !!}
+          </span>
         </div>
       </div>
     </div>
-  </div>
-</section><!-- End Hero -->
-<main id="main">
-  <!-- ======= Nusa Advocates Section ======= -->
-  <section id="Nusa Advocates" class="Nusa Advocates">
-
-    <div class="container" data-aos="fade-up">
-      <div class="section-title p-0">
-        <h2>@lang('messages.news')</h2>
-      </div>
-
-
-      <div class="filter-sections row con" data-aos="fade-up" data-aos-delay="200">
-          @foreach($news as $new)
-          <div class="col-xl-4 col-md-6 align-items-stretch mb-4" data-aos="zoom-in" data-aos-delay="100">
-            <div class="icon-box content box-news">
-              <a href="{{ route('newsdetail', ['lang' => app()->getLocale(), 'slug' => $new->slug]) }}" class="">
-                <div class="img-news text-center mx-auto mb-3">
-                  <img src="{{ asset('storage/news').'/'.$new->image }}" alt="">
-                </div>
-                <div class="p-2">
-                  <h4 class="title-news">{{ $new->title }}</h4>
-                  <span class="date-news">{{ date('F Y', strtotime($new->date )) }} </span>
+    @endforeach
     
-                  <p class="desc-news">{{ $new->short_desc }}
-                  </p>
-                </div>
-              </a>
-            </div>
-          </div>
-          @endforeach
-      </div>
-
-    </div>
-  </section><!-- End Nusa Advocates Section -->
-
+  </section>
 </main><!-- End #main -->
 @endsection
