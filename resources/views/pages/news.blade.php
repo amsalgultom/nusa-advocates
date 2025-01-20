34ebd868a93ec8db7a760f15f678dcae8f6982d7
Amsal Gultom <gultomamsal@gmail.com>
2025-01-20 22:23:33 +0700

    update content news

--- resources/views/pages/news.blade.php
+++ resources/views/pages/news.blade.php
@@ -4,11 +4,13 @@
 @section('content')
 <main id="main">  <!-- ======= Nusa Advocates Section ======= -->
   <section id="Nusa Advocates" class="Nusa Advocates mt-5">
-  @foreach($news as $new)
     <div class="container mt-4">
+      <h4 class="title-newsdetail">Firm News</h4>
+    </div>
+  @foreach($news as $new)
+    <div class="container mt-3">
       <div class="row">
         <div class="col-lg-12">
-          <h4 class="title-newsdetail">{{ $new->title }}</h4>
           <p>{{ date('d F Y', strtotime($new->date )) }}</p>
           <span class="desc-newsdetail">
             {!! $new->desc !!}
