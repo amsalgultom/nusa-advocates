78826eb34bf80f420d2f15a477aa63e869b18978
Amsal Gultom <gultomamsal@gmail.com>
2025-05-16 21:06:32 +0700

    update header news

--- resources/views/layout-views/header.blade.php
+++ resources/views/layout-views/header.blade.php
@@ -17,7 +17,7 @@
           <li><a class="nav-link scrollto {{ (request()->segment(2) == 'practice_area') ? 'active' : '' }}" href="{{ route('practice_area', ['lang' => app()->getLocale()]) }}">@lang('messages.practice_area')</a></li>
           <li><a class="nav-link scrollto {{ (request()->segment(2) == 'principal_lawyer') ? 'active' : '' }}" href="{{ route('principal_lawyer', ['lang' => app()->getLocale()]) }}">@lang('messages.principal_lawyer')</a></li>
           <!-- <li><a class="nav-link scrollto {{ (request()->segment(2) == 'news') ? 'active' : '' }}" href="{{ route('news', ['lang' => app()->getLocale()]) }}">@lang('messages.news')</a></li> -->
-          <li class="dropdown"><a href="#" class="{{ (request()->segment(2) == 'news' || request()->segment(2) == 'update') ? 'active' : '' }}"><span>@lang('messages.news_update')</span> <i class="bi bi-chevron-down toggle-dropdown"></i></a>
+          <li class="dropdown"><a href="#" class="{{ (request()->segment(2) == 'news' || request()->segment(2) == 'update') ? 'active' : '' }}"><span>@lang('messages.news')</span> <i class="bi bi-chevron-down toggle-dropdown"></i></a>
             <ul>
               <li><a href="{{ route('news', ['lang' => app()->getLocale()]) }}">@lang('messages.just_news')</a></li>
               <li><a href="{{ route('update', ['lang' => app()->getLocale()]) }}">@lang('messages.just_update')</a></li>
